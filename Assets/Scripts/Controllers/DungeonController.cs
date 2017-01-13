7040b3ed8b91c352aed133b9f790138ff913812c
AMITHOME\ritoban <ritobanrc@gmail.com>
2017-01-12 19:53:30 -0800

    Added support for normal maps with sprites 	- This KILLS batching. Should look into using texture atlases 	- Added a few text normal maps. These look OK. Maybe we should look into non-pixel art 	- Added basic door sprite (for connectors). Will need to work on alignment 	- Replaced directional light with point light. It looks good with the normal map, but walls don't cast shadows, giving the effect that the light can go through the walls.

--- Assets/Scripts/Controllers/DungeonController.cs
+++ Assets/Scripts/Controllers/DungeonController.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class DungeonController : MonoBehaviour
 {
     Dungeon dungeon;
-    int width = 301;
-    int height = 201;
+    int width = 31;
+    int height = 21;
     int maxRooms = 1024;
     int maxRoomAttempts = 1024;
     int minRoomSize = 4;
@@ -19,7 +19,7 @@ public class DungeonController : MonoBehaviour
     private void Start()
     {
         dungeon = new Dungeon(width, height, maxRooms, maxRoomAttempts, minRoomSize, maxRoomSize, extraConnectorChance, deadEndRemovalChance, 1236);
-        if(dungeon == null)
+        if (dungeon == null)
         {
             // the dungeon generation failed for some reason
             Debug.LogError("DungeonController::Start - The dungeon generation failed!");
@@ -39,43 +39,60 @@ public class DungeonController : MonoBehaviour
         foreach (Sprite s in sprites)
         {
             AllSpritesByName.Add(s.name, s);
+            spriteNrmMap.Add(s, 
+                Resources.Load<Texture>("Sprites/" + s.name + "_nrm")
+            ); // FIXME: Hard coding in name
         }
     }
 
 
     private void CreateGameObjects()
     {
+        // This is just an empty material with a basic bumped sprite shader
         Material diffuseMat = Resources.Load<Material>("Materials/Sprites_Diffuse");
         for (int y = 0; y < dungeon.Height; y++)
         {
+            // Create a parent for each row, just to keep stuff organised
             Transform parent = new GameObject("Row " + y.ToString()).transform;
             parent.parent = this.transform;
-            parent.position = new Vector3(0, y - dungeon.Height/2f + 0.5f);
+            parent.position = new Vector3(0, y - dungeon.Height / 2f + 0.5f);
             for (int x = 0; x < dungeon.Width; x++)
             {
+                // Get the tile from the dungeon
                 Tile tile_data = dungeon.GetTileAt(x, y);
+                // Create an empty game object
                 GameObject tile_obj = new GameObject("Tile " + x + "_" + y);
-                tile_obj.transform.position = new Vector3(x - (dungeon.Width/2f + 0.5f), y - (dungeon.Height / 2f + 0.5f));
+                // Position that gameObject
+                tile_obj.transform.position = new Vector3(x - (dungeon.Width / 2f + 0.5f), y - (dungeon.Height / 2f + 0.5f));
+                // Set the parent accordingly
                 tile_obj.transform.SetParent(parent);
+                // Add a sprite renderer
                 SpriteRenderer sr = tile_obj.AddComponent<SpriteRenderer>();
+                // Assign the sprite. GetSpriteForTile uses some data from the Tile class and finds the appropriate sprite from a dictionary of sprites
                 sr.sprite = GetSpriteForTile(tile_data);
+                // Make sure the sprite renderer is using the bumped diffuse material (should allow batching)
                 sr.sharedMaterial = diffuseMat;
+                // If we successfully found a sprite, check our Dictionary that links sprites and normal maps to assign the appropriate normal map.
+                if(sr.sprite != null && spriteNrmMap[sr.sprite] != null)
+                    sr.material.SetTexture("_BumpMap", spriteNrmMap[sr.sprite]);
             }
         }
     }
 
+    Dictionary<Sprite, Texture> spriteNrmMap = new Dictionary<Sprite, Texture>();
 
     public Sprite GetSpriteForTile(Tile t)
     {
-        if(t.room != null)
+        if (t.room != null)
         {
-            return AllSpritesByName["Cobblestone"];
+            return AllSpritesByName["Cobblestone"]; // FIXME: Hard coding! Should use TileType enum or store sprite name on tile
         }
         if (t.isCorridor)
             return AllSpritesByName["Stones"];
         if (t.IsWall)
             return AllSpritesByName["RockWall"];
-
+        if (t.isConnector)
+            return AllSpritesByName["Door"];
         return null;
     }
 
