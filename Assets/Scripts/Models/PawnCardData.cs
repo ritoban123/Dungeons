a1b48e70b78b623a4f3a8bc34a09df1fab9ae2d1
ritoban123 <ritobanrc@gmail.com>
2017-01-15 10:00:00 -0800

    Pawn management (#2)
    * Created basics for pawn system. Still need to add pathFinding
    
    	- Created simple animations for PawnCard
    	- Added CountText to PawnCard
    	- Changed Map size to 15 by 15 for testing
    	- Creating pawns on click, subtracting from count, running pawn update function and created gameObjects for pawns
    	- Added update function and position to Pawn.cs
    	- Added count to PawnCardData
    	- PawnCard Buttons disable when count reaches zero, tell pawnController when selected
    	- changed sprite paths (in scene)
    	- added scene to build settings. (will asl ohave to do this in master)

---
+++ Assets/Scripts/Models/PawnCardData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PawnCardData
+{
+    public string cardSpritePath = ""; // FIXME: Resources.Load does not work with spritesheets/texture atlases
+    public int count = 1;
+
+    public PawnCardData(string cardSpritePath, int count = 1)
+    {
+        this.cardSpritePath = cardSpritePath;
+        this.count = count;
+    }
+
+}
---
+++ Assets/Scripts/UI/UI_PawnCard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UI_PawnCard : MonoBehaviour
+{
+    [HideInInspector]
+    public PawnCardData data;
+    [HideInInspector]
+    public Sprite sprite;
+
+    public Image backImg;
+    public Text countText;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (backImg.sprite != sprite)
+            backImg.sprite = sprite;
+        countText.text = data.count.ToString();
+
+        if(data.count <= 0)
+        {
+            button.interactable = false;    
+        }
+    }
+
+    Button button;
+    PawnController pc;
+
+    public void OnButtonClicked()
+    {
+        if (pc == null)
+            pc = FindObjectOfType<PawnController>();
+        pc.Selected = data;
+        Debug.Log(sprite.name);
+    }
+}
